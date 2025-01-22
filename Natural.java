997bdf4263dfe4ecd6089ae4eab3fe78e091c5b8
Shahana T P <shahanathirumurugan717@gmail.com>
2025-01-22 19:55:27 +0530

    Add files via upload
    Program to calculate the sum of first 10 natural numbers.

---
+++ Natural.java
@@ -0,0 +1,10 @@
+class Natural {
+public static void main(String[] args) {
+int n = 10;
+int sum=0;
+for(int i=1;i<=n;i++) {
+sum += i;
+}
+System.out.println("The sum of first " + n + " natural numbers are:" + sum);
+}
+}
